3041955c85be4de5bd2f78495fb06ac3c6162166
theradP <mpranjal380@gmail.com>
2023-07-21 05:13:18 +0530

    fixed if conditions

--- djangoProject1/tasks.py
+++ djangoProject1/tasks.py
@@ -9,21 +9,21 @@ def update_task_statuses():
     tasks = Task.objects.all()
 
     for task in tasks:
-        if task.priority == 0 and task.status == 0:
+        if task.priority == 0:
             if (timezone.now() - task.created_at) >= timedelta(seconds=30):
                 task.status = 1
                 task.save()
             elif (timezone.now() - task.created_at) >= timedelta(minutes=1):
                 task.status = 2
                 task.save()
-        elif task.priority == 1 and task.status == 0:
+        elif task.priority == 1:
             if (timezone.now() - task.created_at) >= timedelta(minutes=1):
                 task.status = 1
                 task.save()
             elif (timezone.now() - task.created_at) >= timedelta(minutes=2):
                 task.status = 2
                 task.save()
-        elif task.priority == 2 and task.status == 0:
+        elif task.priority == 2:
             if (timezone.now() - task.created_at) >= timedelta(minutes=2):
                 task.status = 1
                 task.save()
